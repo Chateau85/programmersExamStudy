5690a87f95f48e19ff35cf63f29b7872bdf9445c
cw <cw@DESKTOP-9T5AADF>
2020-06-11 07:36:14 +0900

    42746 보충 commit

--- ProgrammersExamStudy/src/_42746/solution.java
+++ ProgrammersExamStudy/src/_42746/solution.java
@@ -3,6 +3,11 @@ package _42746;
 import java.util.Arrays;
 import java.util.Comparator;
 
+/**
+ * 
+ * @author cw https://programmers.co.kr/learn/courses/30/lessons/42746
+ *
+ */
 public class solution {
 
 	public static void main(String[] args) {
@@ -19,12 +24,12 @@ public class solution {
 		for (int e : numbers) {
 			sb.setLength(0);
 			sb.append(String.valueOf(e));
-			while (sb.length() <= 4) {
+			while (sb.length() <= 4) { // numbers의 숫자를 5-6 자리로 만듬. 66666/10101/22222, 33333/30303/34343/55555/99999
 				sb.append(e);
 			}
 			temp = sb.toString();
-			if (temp.length() > 5) {
-				temp = temp.substring(0, 5);
+			if (temp.length() > 5) { //
+				temp = temp.substring(0, 5); // 반복되어 가공된 숫자를 5자리로 자름
 			}
 			copies[index][0] = Integer.valueOf(temp);
 			copies[index][1] = e;
@@ -36,14 +41,14 @@ public class solution {
 			public int compare(int[] o1, int[] o2) {
 				return o1[0] > o2[0] ? -1 : o1[0] < o2[0] ? 1 : 0;
 			}
-		});
+		}); // 5자리 숫자의 크기로 정렬
 		sb.setLength(0);
-		for (int[] e : copies) {
+		for (int[] e : copies) { // 정렬된 순서대로 원래 numbers의 값 이어붙임
 			sb.append(e[1]);
 		}
 
 		answer = sb.toString();
-		if (answer.charAt(0) == '0') {
+		if (answer.charAt(0) == '0') { // 0 일경우 String 0 으로 변환
 			answer = "0";
 		}
 
